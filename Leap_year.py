6d461400b657da8ac2a30ea1cb5e757aae3208c7
HARIOM26 <72153454+HARIOM26@users.noreply.github.com>
2021-10-01 22:11:11 +0530

    Create Leap_year.py

---
+++ Leap_year.py
@@ -0,0 +1,13 @@
+year = int(input("Enter Year:"))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Yes, {} is Leap Year".format(year))
+        else:
+            print("No, {} is Leap Year".format(year))
+    else:
+        print("No, {} is Leap Year".format(year))
+else:
+    print("No, {} is Leap Year".format(year))
+    
+    
